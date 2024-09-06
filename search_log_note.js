7508803bf6cfddc1e0e40173e9a769e808d70e83
Julien (jula) <jula@odoo.com>
2024-09-06 15:39:47 +0200

    search log not : remove duplicate results

--- search_log_note.js
+++ search_log_note.js
@@ -2,86 +2,117 @@
 
 odoo.define("@search_utils" + Math.random(), ["@web/session"], async function (require) {
   const { session } = require("@web/session");
-  const partner_id = session.storeData.Persona[1].id;
+  const parser = new DOMParser();
 
   async function search_read(model, domain, fields) {
-    return (
-      await fetch("https://www.odoo.com/web/dataset/call_kw/project.task/search_read", {
-        headers: {
-          "content-type": "application/json",
-        },
-        body: JSON.stringify({
-          method: "call",
-          jsonrpc: "2.0",
-          params: {
-            args: [],
-            kwargs: {
-              domain,
-              limit: 2000,
-              offset: 0,
-              order: "",
-              fields,
-            },
-            model,
-            method: "search_read",
+    const response = await fetch("https://www.odoo.com/web/dataset/call_kw/project.task/search_read", {
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        method: "call",
+        jsonrpc: "2.0",
+        params: {
+          args: [],
+          kwargs: {
+            domain,
+            limit: 2000,
+            offset: 0,
+            order: "",
+            fields,
           },
-        }),
-        method: "POST",
-        mode: "cors",
-        credentials: "include",
-      }).then((r) => r.json())
-    ).result;
+          model,
+          method: "search_read",
+        },
+      }),
+      method: "POST",
+      mode: "cors",
+      credentials: "include",
+    }).then((r) => r.json())
+    return response.result;
   }
 
-  async function search(searchString, caseSensitive = false) {
-    const task_ids = (
-      await search_read(
-        "project.task",
-        [["message_follower_ids.partner_id", "=", partner_id]],
-        ["id"]
-      )
-    ).map((r) => r.id);
+  function searchTasks(partnerId) {
+    return search_read(
+      "project.task",
+      [["message_follower_ids.partner_id", "=", partnerId]],
+      ["id"]
+    ).then(r => r.map(t => t.id));
+  }
 
-    const messages = await search_read(
+  function searchMessages(taskIds, searchString, caseSensitive) {
+    return search_read(
       "mail.message",
       [
         ["model", "=", "project.task"],
-        ["res_id", "in", task_ids],
+        ["res_id", "in", taskIds],
         ["body", caseSensitive ? "like" : "ilike", searchString],
       ],
       ["record_name", "res_id", "author_id", "body"]
     );
+  }
 
-    function format(messages) {
-      const parser = new DOMParser();
-      messages
-        .map((m) => {
-          const content = parser.parseFromString(m.body, "text/html").body.textContent;
-        //   const content = m.body;
-          const i = caseSensitive
-            ? content.indexOf(searchString)
-            : content.toLowerCase().indexOf(searchString.toLowerCase());
-          let sub = "…";
-          if (i >= 0) {
-            sub =
-              "…" +
-              content.substring(Math.max(0, i - 80), i) +
-              `**${content.substring(i, i + searchString.length)}**` +
-              content.substring(i + searchString.length, Math.min(content.length, i + 80)) +
-              "…";
-          }
-          return [
-            `%c${m.record_name}`,
-            "font-weight: bold;",
-            `https://www.odoo.com/odoo/49/tasks/${m.res_id}\n`,
-            m.author_id[1] + "\n\n",
-            sub + "\n",
-          ];
-        })
-        .forEach((m) => console.log(...m));
-    }
-    format(messages);
+  function normStr(str) {
+    return str.normalize("NFD").replace(/\p{Diacritic}/gu, "");
+  }
+
+  function formatSearchResult(m, searchString, caseSensitive) {
+    const normSS = normStr(searchString)
+    const content = normStr(parser.parseFromString(m.body, "text/html").body.textContent);
+      const i = caseSensitive
+        ? content.indexOf(normSS)
+        : content.toLowerCase().indexOf(normSS.toLowerCase());
+      let sub = "…%c%c";
+      if (i >= 0) {
+        sub =
+          content.substring(Math.max(0, i - 80), i) +
+          `%c${content.substring(i, i + searchString.length)}%c` +
+          content.substring(i + searchString.length, Math.min(content.length, i + 80));
+
+        sub = "…" + sub.trim() + "…"
+      }
+      return {
+        title: m.record_name,
+        taskId: m.res_id,
+        author: m.author_id[1],
+        text: sub 
+      };
+  }
+
+  function filterDuplicateResults(results) {
+    const newResults = [];
+    results.forEach((r, i) => {
+      if (results.some((other, j) => i < j && other.taskId == r.taskId && other.text == r.text)) return;
+      newResults.push(r);
+    });
+    return newResults;
+  }
+
+  function logResult(result) {
+    console.log(
+      `%c${result.title}%c ` +
+      `https://www.odoo.com/odoo/49/tasks/${result.taskId}\n` +
+      result.author + "\n\n" +
+      result.text + "\n",
+      "font-weight: bold;",
+      "font-weight: normal;",
+      "font-weight: bold;",
+      "font-weight: normal;",
+    );
+  }
+
+  async function search(searchString, caseSensitive = false) {
+    const partnerId = session.storeData.Persona[1].id;
+    const taskIds = await searchTasks(partnerId);
+    const messages = await searchMessages(taskIds, searchString, caseSensitive);
+
+    console.log(messages);
+    let results = messages.map(m => formatSearchResult(m, searchString, caseSensitive));
+    results = filterDuplicateResults(results);
+    results.forEach(logResult);
   }
 
   search("SEARCH_STRING");
+
 });
+console.log('Searching...');
